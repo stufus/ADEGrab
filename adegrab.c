53d5eba8579343bea64dbebf9b4c6da104b18f33
Stuart Morgan <stuart.morgan@mwrinfosecurity.com>
2015-09-14 12:21:50 +0100

    General commit

--- adegrab.c
+++ adegrab.c
@@ -6,17 +6,17 @@ HMENU popup, popup_sub;
 
 void addLog(HWND hDlg, TCHAR *message) {
 	SYSTEMTIME lt;
-	TCHAR buf[100] = { 0 };
+	TCHAR buf[400] = { 0 };
 
 	GetLocalTime(&lt);
-	StringCbPrintf(&buf, 100, TEXT("[%d/%d/%d %d:%d:%d] %s"), lt.wDay, lt.wMonth, lt.wYear, lt.wHour, lt.wMinute, lt.wSecond, message);
+	StringCbPrintf(&buf, 400, TEXT("[%d/%d/%d %d:%d:%d] %s"), lt.wDay, lt.wMonth, lt.wYear, lt.wHour, lt.wMinute, lt.wSecond, message);
 	SendDlgItemMessage(hDlg, LIST_LOG, LB_ADDSTRING, 0, (LPARAM)&buf);
 }
 
 void performADExplorerCapture(HWND hDlg, HMENU menu) {
 	MENUITEMINFO mi = { 0 };
 	HANDLE hFile;
-	TCHAR log[100];
+	TCHAR log[300];
 	DWORD dwWritten;
 
 	HWND ADExplorer;
@@ -31,6 +31,9 @@ void performADExplorerCapture(HWND hDlg, HMENU menu) {
 	DWORD remoteMemory;
 	DWORD err;
 	
+	mi.cbSize = sizeof(MENUITEMINFO);
+	mi.fMask = MIIM_STATE;
+
 	// Retrieve the encompassing window
 	if (ADExplorer = FindWindowExW(NULL, NULL, TEXT("#32770"), TEXT("Search Container"))) {
 
@@ -38,7 +41,7 @@ void performADExplorerCapture(HWND hDlg, HMENU menu) {
 		if (ADExplorerListView = FindWindowExW(ADExplorer, NULL, TEXT("SysListView32"), TEXT("List1"))) {
 
 			// Update log
-			StringCbPrintf(&log, 100, TEXT("Found AD Explorer listbox (Handle: %d)."), ADExplorerListView);
+			StringCbPrintf(&log, 300, TEXT("Found AD Explorer listbox (Handle: %d)."), ADExplorerListView);
 			addLog(hDlg, &log);
 
 			// Now get the process Id and thread ID of the AD Explorer process
@@ -47,12 +50,12 @@ void performADExplorerCapture(HWND hDlg, HMENU menu) {
 				// Obtain privileges that we will need to write to its memory
 				if (remoteProcess = OpenProcess(PROCESS_VM_OPERATION | PROCESS_VM_READ | PROCESS_VM_WRITE, FALSE, processId)) {
 
-					StringCbPrintf(&log, 100, TEXT("Opened AD Explorer process (Handle: %d)."), remoteProcess);
+					StringCbPrintf(&log, 300, TEXT("Opened AD Explorer process (Handle: %d)."), remoteProcess);
 					addLog(hDlg, &log);
 
 					if (itemCount = ListView_GetItemCount(ADExplorerListView)) {
 
-						StringCbPrintf(&log, 100, TEXT("Found %d item(s)."), itemCount);
+						StringCbPrintf(&log, 300, TEXT("Found %d item(s)."), itemCount);
 						addLog(hDlg, &log);
 
 						// Allocate memory in remote process in the format [LVITEM][TCHAR String][NULL]
@@ -84,6 +87,28 @@ void performADExplorerCapture(HWND hDlg, HMENU menu) {
 							// MessageBox for debugging
 							MessageBox(NULL, (LPCWSTR)LVQuery.buffer, NULL, NULL);
 
+							// Write to clipboard?
+							GetMenuItemInfo(menu, MAKEINTRESOURCE(ID_ADEGRAB_CAPTURETOCLIPBOARD), FALSE, &mi);
+							if (mi.fState & MFS_CHECKED) {
+								// Copy to clipboard
+							}
+
+							// Save to file?
+							GetMenuItemInfo(menu, MAKEINTRESOURCE(ID_ADEGRAB_CAPTURETOLOGFILE), FALSE, &mi);
+							if (mi.fState & MFS_CHECKED) {
+								if (hFile = CreateFile(TEXT("adegrab.log"), FILE_GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0)) {
+									SetFilePointer(hFile, 0, NULL, FILE_END);
+									WriteFile(hFile, "STUFUS", 6, &dwWritten, NULL);
+									StringCbPrintf(&log, 100, TEXT("Written %d byte(s) to output file."), dwWritten);
+									CloseHandle(hFile);
+								}
+								else {
+									StringCbPrintf(&log, 100, TEXT("Unable to open output file (Error %d)."), GetLastError());
+								}
+								addLog(hDlg, &log);
+							}
+
+
 						}
 
 						// Clean up
@@ -98,27 +123,6 @@ void performADExplorerCapture(HWND hDlg, HMENU menu) {
 		}
 	} else {
 		addLog(hDlg, TEXT("Unable to find Search Container window."));
-	}
-
-	mi.cbSize = sizeof(MENUITEMINFO);
-	mi.fMask = MIIM_STATE;
-	
-	GetMenuItemInfo(menu, MAKEINTRESOURCE(ID_ADEGRAB_CAPTURETOCLIPBOARD), FALSE, &mi);
-	if (mi.fState & MFS_CHECKED) {
-		// Copy to clipboard
-	}
-
-	GetMenuItemInfo(menu, MAKEINTRESOURCE(ID_ADEGRAB_CAPTURETOLOGFILE), FALSE, &mi);
-	if (mi.fState & MFS_CHECKED) {
-		if (hFile = CreateFile(TEXT("adegrab.log"), FILE_GENERIC_WRITE, FILE_SHARE_READ | FILE_SHARE_WRITE, 0, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, 0)) {
-			SetFilePointer(hFile, 0, NULL, FILE_END);
-			//WriteFile(hFile, "STUFUS", 6, &dwWritten, NULL);
-			StringCbPrintf(&log, 100, TEXT("Written %d byte(s) to output file."), dwWritten);
-			CloseHandle(hFile);
-		} else {
-			StringCbPrintf(&log, 100, TEXT("Unable to open output file (Error %d)."), GetLastError());
-		}
-		addLog(hDlg, &log);
 	}
 
 }
--- adegrab.h
+++ adegrab.h
@@ -8,3 +8,8 @@
 #define CM_TRAY	WM_USER+3
 #define ADEGRAB_IDENTIFIER "ADEGrab v1.0"
 #define MAX_BUFFER_SIZE 100
+
+typedef struct _ADMemInject {
+	LVITEM li;
+	TCHAR buffer[MAX_BUFFER_SIZE + 1];
+} ADMemInject;
